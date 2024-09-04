7ab696a549cfaf964343803789ef1e57ebf114be
Viveak Kudesia <viveakkudesia@gmail.com>
2024-09-04 22:14:11 +0530

    coded fileserver.js

--- week-2/02-nodejs/fileServer.js
+++ week-2/02-nodejs/fileServer.js
@@ -16,6 +16,45 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
+// Endpoint to list all files in the './files/' directory
+app.get("/files", (req, res) => {
+  // Define the path to the directory containing files
+  const directoryPath = path.join(__dirname, './files/');
+  
+  // Read the directory contents
+  fs.readdir(directoryPath, (err, files) => {
+    if (err) {
+      // If there's an error reading the directory, respond with a 500 status code
+      return res.status(500).json({ error: 'Unable to scan directory' });
+    }
+    
+    // Respond with the list of file names
+    res.status(200).json(files);
+  });
+});
 
+// Endpoint to get the content of a specific file
+app.get("/file/:filename", (req, res) => {
+  // Extract the filename from the request parameters
+  const filename = req.params.filename;
+  
+  // Define the full path to the file
+  const filePath = path.join(__dirname, './files/', filename);
+  
+  // Read the content of the file
+  fs.readFile(filePath, "utf-8", (err, data) => {
+    if (err) {
+      // If there's an error reading the file, respond with a 404 status code
+      return res.status(404).send("File not found");
+    }
+    
+    // Respond with the content of the file
+    res.status(200).send(data);
+  });
+});
 
+// Handle any other routes with a 404 response
+app.use((req, res) => {
+  res.status(404).send('Not Found');
+});
 module.exports = app;
